42bf5101447c6224dd064617d4e229f8f7bacd78
Andrii Skorniakov <Andrii.Skorniakov@newfold.com>
2022-06-28 19:15:01 +0200

    progress-report bot to test

--- public_html/progress-report.php
+++ public_html/progress-report.php
@@ -17,40 +17,16 @@ try {
 
     $request = $bot->getUpdate();
     $update = json_decode($request);
-    $known_users = json_decode(file_get_contents(__DIR__ . '/../tmp/progress-report-users.json'), true);
-    $known_users_updated = false;
 
     if (!empty($update->message)) {
         switch ($update->message->text) {
-            case '/start':
-                if (!empty($known_users[$update->message->from->id])) {
-                    $bot->sendText('You are already registered here!', $update->message->from, null);
-
-                    break;
-                }
-
-                $known_users[$update->message->from->id] = $update->message->from;
-                $known_users_updated = true;
-                $bot->sendText("Welcome!\nPlease, choose an action from the menu.", $update->message->from, [
-                    [['text' => 'Hello!'], ['text' => 'Bye!'], ['text'=> 'Nice to meet you.']],
-                    [['text' => 'Bonjour!'], ['text' => 'Au revoir!'], ['text' => 'Enchante.']],
-                ]);
-
-                break;
-            case '/stop':
-                unset($known_users[$update->message->from->id]);
-                $known_users_updated = true;
-
-                break;
             default:
-                $bot->sendText($update->message->text, $update->message->from, null);
+                $bot->message($update->message->from)
+                    ->addText($update->message->text)
+                    ->send();
 
                 break;
         }
-    }
-
-    if ($known_users_updated) {
-        file_put_contents(__DIR__ . '/../tmp/progress-report-users.json', json_encode($known_users));
     }
 } catch (Throwable $e) {
     $logger->log($e->getMessage() . PHP_EOL . $e->getTraceAsString() . PHP_EOL);
