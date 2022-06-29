ff6ac734fa54ccf8b108fa3904cfbbdb2bc72aeb
Andrii Skorniakov <Andrii.Skorniakov@newfold.com>
2022-06-29 18:00:00 +0200

    added the container div

--- public_html/index.php
+++ public_html/index.php
@@ -11,6 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
 <h1>Зареєстровані користувачі, в т.ч. ті, що залишали телефони для зв'язку</h1>
 <?php
 $knownUsers = json_decode(file_get_contents(__DIR__ . '/../tmp/naubade-users.json'), true);
@@ -37,6 +38,7 @@ $countUsers = count($knownUsers);
     }
     ?>
 </table>
+</div>
 </body>
 </html>
 
